61a846ccf95ab68c6c9fdb41e35c0d0c60421670
SaiPraveen912 <saipraveen.immanni@gmail.com>
2023-04-08 14:48:40 +0530

    Affirmation Images and Themes

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -6,16 +6,16 @@ import com.example.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmation(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.Affirmation1),
-            Affirmation(R.string.Affirmation2),
-            Affirmation(R.string.Affirmation3),
-            Affirmation(R.string.Affirmation4),
-            Affirmation(R.string.Affirmation5),
-            Affirmation(R.string.Affirmation6),
-            Affirmation(R.string.Affirmation7),
-            Affirmation(R.string.Affirmation8),
-            Affirmation(R.string.Affirmation9),
-            Affirmation(R.string.Affirmation10)
+            Affirmation(R.string.Affirmation1, R.drawable.image1),
+            Affirmation(R.string.Affirmation2, R.drawable.image2),
+            Affirmation(R.string.Affirmation3, R.drawable.image3),
+            Affirmation(R.string.Affirmation4, R.drawable.image4),
+            Affirmation(R.string.Affirmation5, R.drawable.image5),
+            Affirmation(R.string.Affirmation6, R.drawable.image6),
+            Affirmation(R.string.Affirmation7, R.drawable.image7),
+            Affirmation(R.string.Affirmation8, R.drawable.image8),
+            Affirmation(R.string.Affirmation9, R.drawable.image9),
+            Affirmation(R.string.Affirmation10, R.drawable.image10)
         )
     }
 }
